bdce1b5e6d6611021943a034e334ae0a78bbb701
vcheckzen <18008498+vcheckzen@users.noreply.github.com>
2021-06-24 13:13:47 +0800

    refactor: make installation process repeatable

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 REPO=LanzouCloudAPI
+DOMAIN=pan.lanzou.com
+IP=47.91.203.9
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
 cd /usr/local/share
 curl -LO "https://github.com/vcheckzen/$REPO/archive/master.tar.gz"
 
+rm -rf "$REPO"
 mkdir "$REPO"
 tar xf master.tar.gz -C "$REPO" --strip-components 1
 
@@ -14,7 +17,13 @@ cd "$REPO"
 poetry config virtualenvs.in-project true
 poetry install
 
-echo '47.91.203.9 pan.lanzou.com' >>/etc/hosts
+grep "$DOMAIN" /etc/hosts &>/dev/null && {
+    sed -i "s/.*$DOMAIN/$IP $DOMAIN/" /etc/hosts
+} || {
+    echo "$IP $DOMAIN" >>/etc/hosts
+}
+
+systemctl stop lanzous 2>/dev/null
 
 cp lanzous.service /etc/systemd/system/
 systemctl daemon-reload
